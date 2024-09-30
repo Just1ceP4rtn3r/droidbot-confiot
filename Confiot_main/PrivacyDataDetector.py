1a7ee2de445cbed6d6e4f5cc6dc5a236b30f1263
TracyMonkey <1002482382@qq.com>
2024-09-29 22:50:13 -0400

    add privacy data detector

---
+++ Confiot_main/PrivacyDataDetector.py
@@ -0,0 +1,8 @@
+# Function tests for detecting privacy sensitive data
+# Input: UIComparator directory, privacy sensitive data category
+# Output: Privacy sensitive data detection results
+# Test cases
+#   1. August
+#       key files (with host name): 
+#           output/August/guest/Confiot/Comparation/UIHierarchy/000_to_10_1_Access_and_modify_Edit_House_Owners/158c0f863e2718afeed12ad26e5ef3ee5892e6715d7ebea4af5b052557430ea1.html, 253c3c6111c428bfa34b3ffc1fb61f579dd53d44a7db96fd597a58d5db0ed375.html
+
